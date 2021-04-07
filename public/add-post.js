2891f6e4db6176b63a9325b7f89238e95c74918a
Olivia Faraone <oliviafaraone1@MacBook-Pro-10.local>
2021-04-07 00:37:27 -0700

    complete

--- public/add-post.js
+++ public/add-post.js
@@ -10,20 +10,8 @@ async function newFormHandler(event) {
 
     const title = document.querySelector('input[name="post-title"]').value;
     const post_con = document.querySelector('input[name="post_con"]').value;
-  
-    // const response = await fetch(`/dashboard/create`, {
-    //   method: 'POST',
-    //   body: JSON.stringify({
-    //     title,
-    //     post_con
-    //   }),
-    //   headers: {
-    //     'Content-Type': 'application/json'
-    //   }
-    // });
 
     fetch('/api/posts',{
-        //'/dashboard/create', {
         method: 'POST',
       body: JSON.stringify({
         title,
@@ -34,15 +22,10 @@ async function newFormHandler(event) {
       }
     }).then(function (response) {
         document.location.replace('/dashboard');
-        alert(response.statusText);
+       // alert(response.statusText);
     }).catch(error => console.log(error))
 
-  
-    // if (response.ok) {
-    //   document.location.replace('/dashboard');
-    // } else {
-    //   alert(response.statusText);
-    // }
+
   }
   
    document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
